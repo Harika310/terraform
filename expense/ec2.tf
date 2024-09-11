1fbd55106dff7c2f457a880e0f6098564299c11b
Harika Eravathri <harikaeravathri@gmail.com>
2024-09-11 15:11:55 -0700

    terraform-expense

--- expense/ec2.tf
+++ expense/ec2.tf
@@ -14,3 +14,37 @@ resource "aws_instance" "expense" {
         }
     )
 }
+
+resource "aws_security_group" "allow_ssh1" {
+  name        = "allow_ssh1"
+  description = "Allow ssh1 inbound traffic and all outbound traffic"
+  
+ #outgoing traffic
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+ #incoming traffic
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  } 
+
+  #    tags = {
+  #       Name = "allow_ssh1"
+  #     }
+
+  tags = merge(
+        var.common_tags, 
+        {
+           Name = "allow_ssh1"
+        }
+    )
+} 
--- expense/variables.tf
+++ expense/variables.tf
@@ -12,3 +12,11 @@ variable "instance_names" {
     type = list(string)
     default = ["mysql","backend","frontend"]
 }
+
+variable "zone_id" {
+    default = "Z09622173O6F2OX8NP6KO"
+}
+
+variable "domain_name" {
+    default = "daws-81s.online"
+}
