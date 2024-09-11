01439130c3c152455a123c90690fc0de8d22cb7d
Harika Eravathri <harikaeravathri@gmail.com>
2024-09-11 11:44:24 -0700

    terraform

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,36 +1,34 @@
 resource "aws_security_group" "allow_ssh1" {
-  name        = var.sg_name
-  description = var.sg_description
-  
- #outgoing traffic
- #block
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+    name        = var.sg_name
+    description = var.sg_description
 
- #incoming traffic
-  ingress {
-    from_port        = var.from_port
-    to_port          = var.to_port
-    protocol         = var.protocol
-    cidr_blocks      = var.ingress_cidr
-    ipv6_cidr_blocks = ["::/0"]
-  } 
+    # usually we allow everything in egress
+    # block
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
 
- tags = {
-    Name = "allow_ssh1"
-  }
-} 
+    ingress {
+        from_port        = var.from_port
+        to_port          = var.to_port
+        protocol         = var.protocol
+        cidr_blocks      = var.ingress_cidr #allow from everyone
+        ipv6_cidr_blocks = ["::/0"]
+    }
 
-resource "aws_instance" "terraform" {
+    tags = var.tags
+}
 
-   ami = "var.ami_id" 
-   instance_type = "var.instance_type"
+resource "aws_instance" "backend" {
+
+   ami = var.ami_id 
+   instance_type = var.instance_type
    vpc_security_group_ids = [aws_security_group.allow_ssh1.id] 
    tags = var.tags
+  
 
 }
